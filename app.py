ef1606950b3ed0d56a93fbdd1da5b9ca54309137
Ivan 'lostpointer' Khokhlov <lostpointer@gmail.com>
2020-05-11 03:11:29 +0300

    feat coomon: add sqrt handler

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import math
+
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/sqrt', methods=['GET'])
+def add_message():
+    print(request.args)
+    if 'value' not in request.args:
+        return 'No value in request', 400
+    response = {'result': math.sqrt(float(request.args['value']))}
+    return jsonify(response)
 
 
 if __name__ == '__main__':
